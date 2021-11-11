81956d921c8d92bceee00c20aaaa6ff8face6ce6
Patrick A. Levell <patrick29501@gmx.com>
2021-11-11 09:44:09 -0500

    Testing changes to get this to work

--- key-to-toml.py
+++ key-to-toml.py
@@ -1,15 +1,30 @@
 # key-to-toml.py - Sunday, November 7, 2021
 """ Convert JSON secrets to TOML """
-
+import os
 import toml
+from datetime import datetime
+from os.path import exists
 
-output_file = ".streamlit/secrets.toml"
 
-with open("firestore-key.json") as json_file:
-	json_text = json_file.read()
+def json2toml(input_filename, output_filename):
+	if exists(input_filename):
+		with open(input_filename) as json_file:
+			json_text = json_file.read()
 
-config = {"textkey": json_text}
-toml_config = toml.dumps(config)
+		config = {"textkey": json_text}
+		toml_config = toml.dumps(config)
 
-with open(output_file, "w") as target:
-	target.write(toml_config)
+		if exists(output_filename):
+			os.rename(output_filename, output_filename + '~.%s' % datetime.now().astimezone().timestamp())
+		with open(output_filename, "w") as target:
+			target.write(toml_config)
+
+
+input_filename = "firestore-key.json"
+output_filename = ".streamlit/secrets.toml"
+json2toml(input_filename, output_filename)
+
+
+input_filename = "adminsdk-key.json"
+output_filename = ".streamlit/adminsdk-secrets.toml"
+json2toml(input_filename, output_filename)
--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,20 @@ from google.cloud import firestore
 from google.oauth2 import service_account
 import json
 
+"""
+# Replace:
+db = firestore.Client.from_service_account_json("firestore-key.json")
+# With:
+import json
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="streamlit-reddit")
+"""
+
+key_dict = json.loads(st.secrets["textkey"])
+creds = service_account.Credentials.from_service_account_info(key_dict)
+# db = firestore.Client(credentials=creds, project="streamlit-reddit")
+db = firestore.Client(credentials=creds)
 
 # db = firestore.Client.from_service_account_json("firestore-key.json")
 
